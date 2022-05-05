9115c4225b1b1728dfa59478778712119110e7f9
Alexandra Zueva <guseva-1992@yandex.ru>
2022-05-05 22:45:14 +0300

    add solution 1

---
+++ 001/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти сумму чисел от 1 до А
+
+int S = 0;
+Console.WriteLine("Enter A");
+int A = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= A; i++)
+{   
+    S = S + i;
+}
+Console.WriteLine(S);
